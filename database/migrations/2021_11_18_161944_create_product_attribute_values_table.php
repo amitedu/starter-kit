5b91071f56580c5de4f311217d6f279bf6ef3a61
Amit <amitedu21@gmail.com>
2021-11-19 09:14:19 +0530

    test biosleep database - product

---
+++ database/migrations/2021_11_18_161944_create_product_attribute_values_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductAttributeValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attribute_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id');
+            $table->foreignId('attribute_id');
+            $table->foreignId('attribute_value_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_attribute_values');
+    }
+}
